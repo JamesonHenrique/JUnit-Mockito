81c2415cb7d8c7b2c3f57eaed069f7e10a61e83a
Jameson <jamesonhenrique14@gmail.com>
2025-01-30 01:30:49 -0300

    test: adicionar classe de teste TestAnnotationDemo com exemplo básico

---
+++ src/test/java/com/jhcs/test/annotation/TestAnnotationDemo.java
@@ -0,0 +1,12 @@
+package com.jhcs.test.annotation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestAnnotationDemo {
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
