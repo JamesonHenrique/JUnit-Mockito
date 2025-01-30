4d0dbe02c71a01a4fdab7f5eaacc34612f771fc6
Jameson <jamesonhenrique14@gmail.com>
2025-01-30 02:26:39 -0300

    feat(assertions): adicionar testes com assertFalse para BookService
    - Adicionados vários métodos de teste na classe AssertFalseDemo para verificar se a lista de livros não está vazia usando diferentes variações do assertFalse.
    - Métodos de teste adicionados:
      - assertFalseWithNoMessage
      - assertFalseWithMessage
      - assertFalseWithMessageSupplier
      - assertFalseWithBooleanSupplierAndNoMessage
      - assertFalseWithBooleanSupplierAndMessage
      - assertFalseWithBooleanSupplierAndSupplierMessage

---
+++ src/test/java/com/jhcs/assertions/assertFalse/AssertFalseDemo.java
@@ -0,0 +1,84 @@
+package com.jhcs.assertions.assertFalse;
+
+
+import com.jhcs.bookstore.model.Book;
+import com.jhcs.bookstore.service.BookService;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class AssertFalseDemo {
+    @Test
+    public void assertFalseWithNoMessage() {
+        BookService bookService = new BookService();
+        Book headFirstJavaBook = new Book(
+                "1",
+                "Head First Java",
+                "O'Reilly"
+        );
+        bookService.addBook(headFirstJavaBook);
+        List<Book> listOfBooks = bookService.books();
+        assertFalse(listOfBooks.isEmpty());
+    }
+    @Test
+    public void assertFalseWithMessage() {
+        BookService bookService = new BookService();
+        Book headFirstJavaBook = new Book(
+                "1",
+                "Head First Java",
+                "O'Reilly"
+        );
+        bookService.addBook(headFirstJavaBook);
+        List<Book> listOfBooks = bookService.books();
+        assertFalse(listOfBooks.isEmpty(), "A lista de livros está vazia");
+    }
+    @Test
+    public void assertFalseWithMessageSupplier() {
+        BookService bookService = new BookService();
+        Book headFirstJavaBook = new Book(
+                "1",
+                "Head First Java",
+                "O'Reilly"
+        );bookService.addBook(headFirstJavaBook);
+        List<Book> listOfBooks = bookService.books();
+        assertFalse(listOfBooks.isEmpty(), () -> "A lista de livros está vazia");
+    }
+    @Test
+    public void assertFalseWithBooleanSupplierAndNoMessage() {
+        BookService bookService = new BookService();
+        Book headFirstJavaBook = new Book(
+                "1",
+                "Head First Java",
+                "O'Reilly"
+        );
+        bookService.addBook(headFirstJavaBook);
+        List<Book> listOfBooks = bookService.books();
+        assertFalse(() -> listOfBooks.isEmpty());
+    }
+    @Test
+    public void assertFalseWithBooleanSupplierAndMessage() {
+        BookService bookService = new BookService();
+        Book headFirstJavaBook = new Book(
+                "1",
+                "Head First Java",
+                "O'Reilly"
+        );
+        bookService.addBook(headFirstJavaBook);
+        List<Book> listOfBooks = bookService.books();
+        assertFalse(() -> listOfBooks.isEmpty(), "A lista de livros está vazia");
+    }
+    @Test
+    public void assertFalseWithBooleanSupplierAndSupplierMessage() {
+        BookService bookService = new BookService();
+        Book headFirstJavaBook = new Book(
+                "1",
+                "Head First Java",
+                "O'Reilly"
+        );
+        bookService.addBook(headFirstJavaBook);
+        List<Book> listOfBooks = bookService.books();
+        assertFalse(() -> listOfBooks.isEmpty(), () -> "A lista de livros está vazia");
+    }
+}
