0ee00b6ae7565a68aa900a270d8e96435af060e7
Jameson <jamesonhenrique14@gmail.com>
2025-02-05 19:09:55 -0300

    test: adicionar uso de Mockito nos métodos de teste com Mock
    Atualizados os métodos de teste na classe `MockTest` para utilizar Mockito.
    
    Alterações realizadas:
    - `src/test/java/com/jhcs/mockito/testDoubles/mock/MockTest.java`: adicionado método `demoMockWithMockito` que utiliza Mockito para criar um mock de `BookRepository` e verificar o comportamento do `BookService`.
    
    Métodos de teste adicionados:
    - `demoMockWithMockito`: valida a adição de livros e a verificação de chamadas ao método `save` no repositório utilizando um mock de `BookRepository`.

--- src/test/java/com/jhcs/mockito/testDoubles/mock/MockTest.java
+++ src/test/java/com/jhcs/mockito/testDoubles/mock/MockTest.java
@@ -1,22 +1,31 @@
 package com.jhcs.mockito.testDoubles.mock;
 
 
-import com.jhcs.mockito.testDoubles.spy.Book;
-import com.jhcs.mockito.testDoubles.spy.BookService;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class MockTest {
     @Test
     public void demoMock() {
         BookRepositoryMock bookRepositoryMock = new BookRepositoryMock();
         BookService bookService = new BookService(bookRepositoryMock);
-        Book book1 = new com.jhcs.mockito.testDoubles.spy.Book("1", "The First Java", 500, LocalDate.now());
+
+        Book book1 = new Book("1", "The First Java", 500, LocalDate.now());
         Book book2 = new Book("2", "The First Spring", 450, LocalDate.now()); bookService.addBook(book1);
         bookService.addBook(book2); bookService.addBook(book1);
         bookRepositoryMock.verify(book2,1);
+    }
+    @Test
+    public void demoMockWithMockito() {
+        BookRepository bookRepository  = mock(BookRepository.class);
+        BookService bookService = new BookService(bookRepository);
+        Book book1 = new Book("1", "The First Java", 500, LocalDate.now());
+        Book book2 = new Book("2", "The First Spring", 450, LocalDate.now()); bookService.addBook(book1);
+        bookService.addBook(book2); bookService.addBook(book1);
+        verify(bookRepository, times(1)).save(book2);
     }
 }
