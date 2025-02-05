66f9beb78ab18336cf851a2880264db694aa26a4
Jameson <jamesonhenrique14@gmail.com>
2025-02-05 19:15:43 -0300

    test: adicionar uso de Mockito nos métodos de teste com Spy
    Atualizados os métodos de teste na classe `SpyTest` para utilizar Mockito.
    
    Alterações realizadas:
    - `src/test/java/com/jhcs/mockito/testDoubles/spy/SpyTest.java`: adicionado método `demoSpyWithMockito` que utiliza Mockito para criar um spy de `BookRepository` e verificar o comportamento do `BookService`.
    
    Métodos de teste adicionados:
    - `demoSpyWithMockito`: valida a adição de livros e a verificação de chamadas ao método `save` no repositório utilizando um spy de `BookRepository`.

--- src/test/java/com/jhcs/mockito/testDoubles/spy/SpyTest.java
+++ src/test/java/com/jhcs/mockito/testDoubles/spy/SpyTest.java
@@ -7,18 +7,27 @@ import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 public class SpyTest {
     @Test
     public void demoSpy() {
         BookRepositorySpy bookRepositorySpy = new BookRepositorySpy();
         BookService bookService = new BookService(bookRepositorySpy);
-        Book book1 =    new Book("1", "The First Java",500, LocalDate.now());
-        Book book2 =   new Book("2", "The First Spring", 450, LocalDate.now());
-        bookService.addBook(book1);
+        Book book1 = new Book("1", "The First Java", 500, LocalDate.now());
+        Book book2 = new Book("2", "The First Spring", 450, LocalDate.now()); bookService.addBook(book1);
         bookService.addBook(book2);
 
         assertEquals(1, bookRepositorySpy.timesCalled());
 
+    }
+
+    @Test
+    public void demoSpyWithMockito() {
+        BookRepository bookRepository = spy(BookRepository.class);
+        BookService bookService = new BookService(bookRepository);
+        Book book1 = new Book("1", "The First Java", 500, LocalDate.now());
+        Book book2 = new Book("2", "The First " + "Spring", 450, LocalDate.now()); bookService.addBook(book1);
+        bookService.addBook(book2); bookService.addBook(book1); verify(bookRepository, times(1)).save(book2);
     }
 }
