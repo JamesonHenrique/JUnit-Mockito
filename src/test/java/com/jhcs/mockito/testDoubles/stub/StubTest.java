bf993c0de35baf3dfa4b53b3ed19b8d74d85d7f4
Jameson <jamesonhenrique14@gmail.com>
2025-02-05 18:56:03 -0300

    test: adicionar uso de Mockito nos métodos de teste com Stub
    Atualizados os métodos de teste na classe `StubTest` para utilizar Mockito.
    
    Alterações realizadas:
    - `src/test/java/com/jhcs/mockito/testDoubles/stub/StubTest.java`: adicionado método `testStubWithMockito` que utiliza Mockito para criar um mock de `BookRepository` e verificar o comportamento do `BookService`.
    
    Métodos de teste adicionados:
    - `testStubWithMockito`: valida a aplicação de desconto em livros novos utilizando um mock de `BookRepository`.

--- src/test/java/com/jhcs/mockito/testDoubles/stub/StubTest.java
+++ src/test/java/com/jhcs/mockito/testDoubles/stub/StubTest.java
@@ -2,19 +2,42 @@ package com.jhcs.mockito.testDoubles.stub;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class StubTest {
     @Test
     public void demoStub() {
         BookRepository bookRepository = new BookRepositoryStub();
         BookService bookService = new BookService(bookRepository);
+
         List<Book> newBooksWithAppliedDiscount = bookService.getNewBooksWithAppliedDiscount(10,7);
+
         assertEquals(2, newBooksWithAppliedDiscount.size());
         assertEquals(450,newBooksWithAppliedDiscount.get(0).getPrice());
         assertEquals(405,newBooksWithAppliedDiscount.get(1).getPrice());
 
+    }
+    @Test
+    public void testDummyWithMockito() {
+        BookRepository bookRepository = mock(BookRepository.class);
+        BookService bookService = new BookService(bookRepository);
+        Book book1 =  new Book("1", "Head First Java", 500, LocalDate.now());
+        Book book2 = new Book("2", "Head First Spring", 450, LocalDate.now());
+        List<Book> newBooks = new ArrayList<>();
+        newBooks.add(book1);
+        newBooks.add(book2);
+        when(bookRepository.findNewBooks(7)).thenReturn(newBooks);
+        List<Book> newBooksWithAppliedDiscount = bookService.getNewBooksWithAppliedDiscount(10,7);
+
+        assertEquals(2, newBooksWithAppliedDiscount.size());
+        assertEquals(450,newBooksWithAppliedDiscount.get(0).getPrice());
+        assertEquals(405,newBooksWithAppliedDiscount.get(1).getPrice());
     }
 }
