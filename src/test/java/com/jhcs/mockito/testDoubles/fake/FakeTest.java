f41b332b65792e59c73ed476354e76d46e52ca4a
Jameson <jamesonhenrique14@gmail.com>
2025-02-05 18:39:56 -0300

    test: adicionar uso de Mockito nos métodos de teste com Fake
    Atualizados os métodos de teste na classe `FakeTest` para utilizar Mockito.
    
    Alterações realizadas:
    - `src/test/java/com/jhcs/mockito/testDoubles/fake/FakeTest.java`: adicionado método `testFakeWithMockito` que utiliza Mockito para criar um mock de `BookRepository` e verificar o comportamento do `BookService`.
    
    Métodos de teste adicionados:
    - `testFakeWithMockito`: valida a adição de livros e a contagem de livros no serviço utilizando um mock de `BookRepository`.

--- src/test/java/com/jhcs/mockito/testDoubles/fake/FakeTest.java
+++ src/test/java/com/jhcs/mockito/testDoubles/fake/FakeTest.java
@@ -3,9 +3,14 @@ package com.jhcs.mockito.testDoubles.fake;
 
 import org.junit.jupiter.api.Test;
 
+
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class FakeTest {
     @Test
@@ -14,6 +19,18 @@ public class FakeTest {
         BookService bookService = new BookService(bookRepository);
         bookService.addBook(new Book("1", "Head First Java", 250, LocalDate.now()));
         bookService.addBook(new Book("2", "Head First Spring", 270, LocalDate.now()));
+        assertEquals(2, bookService.findNumberOfBooks());
+    }
+    @Test
+    public void testFakeWithMockito() {
+        BookRepository bookRepository = mock(BookRepository.class);
+        BookService bookService = new BookService(bookRepository);
+        Book book1 =  new Book("1", "Head First Java", 250, LocalDate.now());
+        Book book2 = new Book("2", "Head First Spring", 270, LocalDate.now());
+        Collection<Book> books = new ArrayList<>();
+        books.add(book1);
+        books.add(book2);
+        when(bookRepository.findAll()).thenReturn(books);
         assertEquals(2, bookService.findNumberOfBooks());
     }
 }
